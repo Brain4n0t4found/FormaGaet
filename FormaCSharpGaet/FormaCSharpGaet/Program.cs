98aea3d765c19ad78aeb0a36ac0ebf2464e052a8
Kélio Steiner <kepo.steiner@yahoo.fr>
2022-06-01 20:44:46 +0200

    Création projet console

---
+++ FormaCSharpGaet/FormaCSharpGaet/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FormaCSharpGaet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
