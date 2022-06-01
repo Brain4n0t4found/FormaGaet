34d06e72878e23427bee18bc010171d87f2e1634
Gaëtan <gaetanfallot67@gmail.com>
2022-06-01 21:48:22 +0200

    Mise en place Character

---
+++ FormaCSharpGaet/FormaCSharpGaet/Model/Character/Character.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormaCSharpGaet.Model.Character
+{
+    internal class Character
+    {
+        #region model properties
+        public string Name { get; set; }
+        private string DecoratedName 
+        { 
+            get 
+            { 
+            
+                return Name+"bitedenoir";
+            } 
+        }
+        public int Health { get; set; }
+        public decimal Strength { get; set; }
+        #endregion
+
+        #region constructor
+        public Character (string name, int health, decimal strenght)
+        {
+            this.Name = name;
+            this.Health = health;
+            this.Strength = strenght;
+        }
+
+        public Character() { }
+        #endregion
+
+    }
+}
--- FormaCSharpGaet/FormaCSharpGaet/Program.cs
+++ FormaCSharpGaet/FormaCSharpGaet/Program.cs
@@ -1,11 +1,19 @@
-﻿namespace FormaCSharpGaet
+﻿using FormaCSharpGaet.Model.Character;
+
+namespace FormaCSharpGaet
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("bite :smirk:");
             Console.WriteLine("Hello, World!");
+            Character perso = new Character("KéBite", 5, 4);
+            Character deuxiemeCharacter = new Character
+            {
+                Name = "tarace",
+                Health = 50,
+                Strength = 2.1M,
+            };
         }
     }
 }
